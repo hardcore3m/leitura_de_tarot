d6dc76312b73cb715bfa295ea0973707701e6c26
Rafael Marins <rafaelcunha@email.com>
2020-12-09 17:26:17 -0300

    Iniciando Aplicação

---
+++ scripts.js
@@ -0,0 +1,26 @@
+/*
+Recebendo informação do usuário
+- Combobox recebe opção do usuário
+- Variável recebe combobox
+- Mostra uma div com explicação do método selecionado
+- Solicita motivação da leitura(pode ser um alert após confirmação do botão)
+- Botão para confirmar seleção
+*/
+var comboCidades = document.getElementById("metodos");
+
+
+
+/*
+Função para sortear cartas
+-Sorteia um numero de 0 a 22 sem repetir
+-recebe os valores sorteados em um array ou variável
+*/
+
+/*
+Imprimindo resultado
+- imprime a data
+- imprime método de leitura
+- imprime a questão
+- Imprime a imagem do arcano
+- Imprime os id's das divs dos numeros armazenados no valor da array ou variáveis
+*/
